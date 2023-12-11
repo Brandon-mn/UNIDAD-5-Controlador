3ba326cd6ef49c59b25d06e2c240f5f3dec0eb56
costi <costi@DESKTOP-886NS64>
2023-12-11 11:00:01 -0600

    Agregar archivos de proyecto.

---
+++ UNIDAD 5 CLIENTE/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Produces("application/xml", "application/json")] // Specifies that the service produces XML
+        public ActionResult<string> GetMessage()
+        {
+            // Creates an anonymous object with the message
+            var xmlResponse = new { Message = "hola" };
+
+            // Returns an OkObjectResult with the anonymous object
+            return Ok(xmlResponse);
+        }
+    }
+}
